413296f4c37f886704a1bb38446155c63944b3b4
Nikalaj87 <traderok@ukr.net>
2023-03-14 17:24:19 +0100

    777

--- src/javatask/hometask/task2/git/homeTask1_Attempt2/FillPingPongStyleDemo.java
+++ src/javatask/hometask/task2/git/homeTask1_Attempt2/FillPingPongStyleDemo.java
@@ -4,6 +4,6 @@ public class FillPingPongStyleDemo {
     public static void main(String[] args) {
         FillArrayPingPong obj = new FillArrayPingPong(new int[6][21],2,3);
         obj.fillArrayPingPongStyle();
-        obj.printArray();
+//        obj.printArray();
     }
 }
