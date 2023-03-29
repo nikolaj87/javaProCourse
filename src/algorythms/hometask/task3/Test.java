7583eca4d90d83a1abeba93f370b7517802ad978
Nikalaj87 <traderok@ukr.net>
2023-03-29 08:44:20 +0200

    777

--- src/algorythms/hometask/task3/Test.java
+++ src/algorythms/hometask/task3/Test.java
@@ -2,13 +2,17 @@ package algorythms.hometask.task3;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(recurs(7));
-
+        int x = 1680;
+        int y = 640;
+        System.out.println(recurs(x, y));
     }
 
-    static int recurs (int n) {
-        if (n==1) return 1;
-        return   (2 * recurs(n - 1) + 1);
+    static int recurs (int x, int y) {
+        int delitel = x-1;
+
+        while(y % delitel != 0 || x % delitel !=0)
+            delitel--;
+        return delitel;
     }
 
 }
