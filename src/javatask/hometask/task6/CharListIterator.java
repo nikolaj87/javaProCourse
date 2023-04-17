8d2893ca9ba81c3887e1f54feff675f0ea672397
Nikalaj87 <traderok@ukr.net>
2023-04-17 18:06:08 +0200

    algorithms home5

--- src/javatask/hometask/task6/CharListIterator.java
+++ src/javatask/hometask/task6/CharListIterator.java
@@ -4,13 +4,13 @@ public class CharListIterator {
 
     public static void main(String[] args) {
 
-        String word1 = "###1#java##";
+        String word1 = "###12#java##";
         String word2 = "ja";
 
-        System.out.println("одинаковые ли слова после приведения первого? - " + function(word1, word2));
+        System.out.println("одинаковые ли слова после модификации первого? - " + function(word1, word2));
     }
 
-    static boolean function(String word1, String word2) {
+    private static boolean function(String word1, String word2) {
 
         int counter = 0;
         String newStrMirror = "";
@@ -18,13 +18,11 @@ public class CharListIterator {
         for (int i = word1.length() - 1; i >= 0; i--) {        //пойдем с конца слова так проще чем угадывать что там будет и чтото запоминать
             if (word1.charAt(i) == '#') {                      //нашли решетку? Записали ее в счетчик чтоб позже удалить символ
                 counter++;
-                continue;
             }
-            if (counter > 0 && word1.charAt(i) != '#') {     //нашли символ? но его надо удалить ведь счетчик решеток положительный
+            else if (counter > 0 && word1.charAt(i) != '#') {     //нашли символ? но его надо удалить ведь счетчик решеток положительный
                 counter--;
-                continue;
             }
-            if (counter == 0 && word1.charAt(i) != '#') {   //нашли символ и счетчик решеток равен ноль? Запишем символ в новое слово
+            else if (counter == 0 && word1.charAt(i) != '#') {   //нашли символ и счетчик решеток равен ноль? Запишем символ в новое слово
                 newStrMirror += word1.charAt(i);
             }
         }
