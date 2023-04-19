4062e784a31c210ce79dca981aa4790239a927f5
Nikalaj87 <traderok@ukr.net>
2023-04-19 11:27:14 +0200

    algorithms home5

--- src/javatask/hometask/adds/HomeExample.java
+++ src/javatask/hometask/adds/HomeExample.java
@@ -4,51 +4,25 @@ package javatask.hometask.adds;
 
     public class HomeExample {
         public static void main(String[] args) {
-            //по две равные стринги
-            String str = "Java##";
-            String str_str = "Ja";
 
-            String str2 = "Ja#va###";
-            String str2str = "";
-
-            String str4 = "##Ja####va#";
-            String str4str = "v";
-
-            String str5="#Hello#World#";
-            String str55 = "HellWorl";
-
-            getWorld(str, str_str);
-            getWorld(str2, str2str);
-            getWorld(str4, str4str);
-            getWorld(str5,str55);
+            System.out.println(getWord("Java##", "Ja"));
+            System.out.println(getWord("Ja#va###", ""));
+            System.out.println(getWord("##Ja####va#", "v"));
+            System.out.println(getWord("#Hello#World#","HellWorl"));
 
         }
 
-        public static String getWorld(String str, String str2) {
+        public static boolean getWord(String str, String str2) {
             String newString = "";
+
             for (int i = 0; i < str.length(); i++) {
-
-                char currentChar = str.charAt(i);
-
-                if (currentChar == '#' && newString.length() > 0) {
+                if (str.charAt(i) == '#' && newString.length() > 0) {
                     newString = newString.substring(0, newString.length() - 1);
-                } else if (currentChar == '#') {
-                    continue;
-                } else {
-                    newString += currentChar;
-
+                } else if (str.charAt(i) != '#'){
+                    newString += str.charAt(i);
                 }
             }
-            System.out.println(newString);
-
-            if (newString.equals(str2)) {
-                System.out.println("строки одинаковые");
-            } else {
-                System.out.println("строки не одинаковые");
-            }
-
-
-            return newString;
+            return newString.equals(str2);
         }
     }
 
