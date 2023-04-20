8749c63282df4b28daa0972479d2feec42f6f47a
Nikalaj87 <traderok@ukr.net>
2023-04-20 14:24:07 +0200

    algorithms home6 add quickSort List

--- src/algorythms/hometask/task6/QuickSort.java
+++ src/algorythms/hometask/task6/QuickSort.java
@@ -6,41 +6,55 @@ public class QuickSort {
 
     public static void main(String[] args) {
 
-        int[] array = {11, 6, 2, 12, 16, 4, 21, 8, 4, 12, 4, 3, 11, 11};
+        /* реализация подобная той что была на уроке сделана со списками.
+        После окончания цикла все элементы меньше array[0] (я не завожу переменную опорную
+        но этот элемент будет по умолчанию опорным) окажутся слева а те что больоше - справа.
+        Индекс также будет известен. Теперь НО.
+
+        рекурсивно не удалось вызывать метод. Ведь когда отрабатывает левая часть массива,
+        она меняет все поинтеры i j и я не пойму как вызвать старые поинтеры для того чтоб
+        запустить рекурсию в правой части списка.
+        Пришлось поменять логику на более линейную как в QuickSort2
+         */
+
+        int[] array = {5, 1, 3, 7, 9, 0, 1};
         System.out.println("array before sort ");
         System.out.println(Arrays.toString(array));
         function(array, 0, array.length - 1);
-
     }
 
-    static int[] function(int[] array, int start, int end) {
-        int[] newArray = new int[(end + 1) - start ];
+    static void function(int[] array, int start, int end) {
+
+        if (start >= end) return;
+
         int i = start;
         int j = end;
-        int middle = array[start];
-        System.out.println("начало i = " + i + " " + " конец j = " + j + " опорный элем = " + middle);
 
-        for (int k = start; k <= end; k++) {
-            if (array[k] < middle) {
-                newArray[i++] = array[k];
-            } else if (array[k] > middle) {
-                newArray[j--] = array[k];
+        int temp;
+        while (i < j) {
+            if (array[j] < array[i]) {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+
+                while (i < j) {
+                    if (array[i] > array[j]) {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                        break;
+                    }
+                    i++;
+                }
             }
-
+            j--;
         }
 
-        for (int k = i; k <= j; k++) {
-            newArray[k] = middle;
-        }
-        System.out.println("array after sort");
-        System.out.println(Arrays.toString(newArray));
-        System.out.println("опорный элемент имеет индекс " + i);
-        System.out.println("последный индекс опорного элемента имеет индекс " + j);
+//        function(array, start, i);
+//        function(array, j, end);
 
-//        function(newArray, 0, i-1);
-//        function(array2, j, end);
-
-        return array;
+        System.out.println("array before sort ");
+        System.out.println(Arrays.toString(array));
     }
 
 }
